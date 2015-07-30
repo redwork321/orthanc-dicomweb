bb3f331a5a5737b0acbcdba0bc97d234edcb6cf4
Sebastien Jodogne <s.jodogne@gmail.com>
2015-07-30 15:57:34 +0200

    fix

--- Resources/SyncOrthancFolder.py
+++ Resources/SyncOrthancFolder.py
@@ -7,9 +7,13 @@
 
 import os
 import shutil
+import urllib2
+
+PLUGIN_SDK_VERSION = '0.9.1'
 
 SOURCE = '/home/jodogne/Subversion/Orthanc'
 TARGET = os.path.join(os.path.dirname(__file__), '..', 'Orthanc')
+REPOSITORY = 'https://bitbucket.org/sjodogne/orthanc/raw/Orthanc-%s/Plugins/Include' % PLUGIN_SDK_VERSION
 
 FILES = [
     'Plugins/Samples/Common/VersionScript.map',
@@ -22,9 +26,15 @@ FILES = [
     'Resources/MinGW-W64-Toolchain32.cmake',
     'Resources/MinGW-W64-Toolchain64.cmake',
     'Resources/MinGWToolchain.cmake',
+    'Resources/ThirdParty/VisualStudio/stdint.h',
     'Resources/WindowsResources.py',
     'Resources/WindowsResources.rc',
 ]
+
+SDK = [
+    'orthanc/OrthancCPlugin.h',
+]   
+
 
 for f in FILES:
     source = os.path.join(SOURCE, f)
@@ -35,3 +45,14 @@ for f in FILES:
         pass
 
     shutil.copy(source, target)
+
+for f in SDK:
+    source = '%s/%s' % (REPOSITORY, f)
+    target = os.path.join(TARGET, 'Sdk-%s' % PLUGIN_SDK_VERSION, f)
+    try:
+        os.makedirs(os.path.dirname(target))
+    except:
+        pass
+
+    with open(target, 'w') as g:
+        g.write(urllib2.urlopen(source).read())
