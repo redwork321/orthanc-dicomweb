4f19832af2c92a22bd87f863340a100934708afe
Sebastien Jodogne <s.jodogne@gmail.com>
2015-08-13 12:52:32 +0200

    sync

---
+++ Plugin/Plugin.cpp
@@ -0,0 +1,208 @@
+/**
+ * Orthanc - A Lightweight, RESTful DICOM Store
+ * Copyright (C) 2012-2015 Sebastien Jodogne, Medical Physics
+ * Department, University Hospital of Liege, Belgium
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ **/
+
+
+#include "Plugin.h"
+
+#include "QidoRs.h"
+#include "StowRs.h"
+#include "WadoRs.h"
+#include "Wado.h"
+#include "Configuration.h"
+
+
+#include <gdcmDictEntry.h>
+#include <gdcmDict.h>
+#include <gdcmDicts.h>
+#include <gdcmGlobal.h>
+
+
+// Global state
+OrthancPluginContext* context_ = NULL;
+Json::Value configuration_;
+const gdcm::Dict* dictionary_ = NULL;
+
+
+static int32_t SwitchStudies(OrthancPluginRestOutput* output,
+                             const char* url,
+                             const OrthancPluginHttpRequest* request)
+{
+  switch (request->method)
+  {
+    case OrthancPluginHttpMethod_Get:
+      // This is QIDO-RS
+      return SearchForStudies(output, url, request);
+
+    case OrthancPluginHttpMethod_Post:
+      // This is STOW-RS
+      return StowCallback(output, url, request);
+
+    default:
+      OrthancPluginSendMethodNotAllowed(context_, output, "GET,POST");
+      return 0;
+  }
+}
+
+
+static int32_t SwitchStudy(OrthancPluginRestOutput* output,
+                           const char* url,
+                           const OrthancPluginHttpRequest* request)
+{
+  switch (request->method)
+  {
+    case OrthancPluginHttpMethod_Get:
+      // This is WADO-RS
+      return RetrieveDicomStudy(output, url, request);
+
+    case OrthancPluginHttpMethod_Post:
+      // This is STOW-RS
+      return StowCallback(output, url, request);
+
+    default:
+      OrthancPluginSendMethodNotAllowed(context_, output, "GET,POST");
+      return 0;
+  }
+}
+
+
+static void Register(const std::string& root,
+                     const std::string& uri,
+                     OrthancPluginRestCallback callback)
+{
+  assert(!uri.empty() && uri[0] != '/');
+  std::string s = root + uri;
+  OrthancPluginRegisterRestCallback(context_, s.c_str(), callback);
+}
+
+
+
+extern "C"
+{
+  ORTHANC_PLUGINS_API int32_t OrthancPluginInitialize(OrthancPluginContext* context)
+  {
+    context_ = context;
+
+    /* Check the version of the Orthanc core */
+    if (OrthancPluginCheckVersion(context_) == 0)
+    {
+      char info[1024];
+      sprintf(info, "Your version of Orthanc (%s) must be above %d.%d.%d to run this plugin",
+              context_->orthancVersion,
+              ORTHANC_PLUGINS_MINIMAL_MAJOR_NUMBER,
+              ORTHANC_PLUGINS_MINIMAL_MINOR_NUMBER,
+              ORTHANC_PLUGINS_MINIMAL_REVISION_NUMBER);
+      OrthancPluginLogError(context_, info);
+      return -1;
+    }
+
+    {
+      std::string version(context_->orthancVersion);
+      if (version == "0.9.1")
+      {
+        OrthancPluginLogWarning(context_, "If using STOW-RS, the DICOMweb plugin can lead to "
+                                "deadlocks in Orthanc version 0.9.1. Please upgrade Orthanc!");
+      }
+    }
+
+
+    OrthancPluginSetDescription(context_, "Implementation of DICOM Web (QIDO-RS, STOW-RS and WADO-RS) and WADO.");
+
+    // Read the configuration
+    dictionary_ = &gdcm::Global::GetInstance().GetDicts().GetPublicDict();
+
+    configuration_ = Json::objectValue;
+
+    {
+      Json::Value tmp;
+      if (!OrthancPlugins::Configuration::Read(tmp, context) ||
+          tmp.type() != Json::objectValue)
+      {
+        OrthancPluginLogError(context_, "Unable to read the configuration file");
+        return -1;
+      }
+
+      if (tmp.isMember("DicomWeb") &&
+          tmp["DicomWeb"].type() == Json::objectValue)
+      {
+        configuration_ = tmp["DicomWeb"];
+      }
+    }
+
+    // Configure the DICOMweb callbacks
+    if (OrthancPlugins::Configuration::GetBoolValue(configuration_, "Enable", true))
+    {
+      std::string root = OrthancPlugins::Configuration::GetRoot(configuration_);
+
+      std::string message = "URI to the DICOMweb REST API: " + root;
+      OrthancPluginLogWarning(context_, message.c_str());
+
+      Register(root, "instances", SearchForInstances);    
+      Register(root, "series", SearchForSeries);    
+      Register(root, "studies", SwitchStudies);
+      Register(root, "studies/([^/]*)", SwitchStudy);
+      Register(root, "studies/([^/]*)/instances", SearchForInstances);    
+      Register(root, "studies/([^/]*)/metadata", RetrieveStudyMetadata);
+      Register(root, "studies/([^/]*)/series", SearchForSeries);    
+      Register(root, "studies/([^/]*)/series/([^/]*)", RetrieveDicomSeries);
+      Register(root, "studies/([^/]*)/series/([^/]*)/instances", SearchForInstances);    
+      Register(root, "studies/([^/]*)/series/([^/]*)/instances/([^/]*)", RetrieveDicomInstance);
+      Register(root, "studies/([^/]*)/series/([^/]*)/instances/([^/]*)/bulk/(.*)", RetrieveBulkData);
+      Register(root, "studies/([^/]*)/series/([^/]*)/instances/([^/]*)/metadata", RetrieveInstanceMetadata);
+      Register(root, "studies/([^/]*)/series/([^/]*)/metadata", RetrieveSeriesMetadata);
+    }
+    else
+    {
+      OrthancPluginLogWarning(context_, "DICOMweb support is disabled");
+    }
+
+    // Configure the WADO callback
+    if (OrthancPlugins::Configuration::GetBoolValue(configuration_, "EnableWado", true))
+    {
+      std::string wado = OrthancPlugins::Configuration::GetWadoRoot(configuration_);
+
+      std::string message = "URI to the WADO API: " + wado;
+      OrthancPluginLogWarning(context_, message.c_str());
+
+      OrthancPluginRegisterRestCallback(context_, wado.c_str(), WadoCallback);
+    }
+    else
+    {
+      OrthancPluginLogWarning(context_, "WADO support is disabled");
+    }
+
+    return 0;
+  }
+
+
+  ORTHANC_PLUGINS_API void OrthancPluginFinalize()
+  {
+  }
+
+
+  ORTHANC_PLUGINS_API const char* OrthancPluginGetName()
+  {
+    return "dicom-web";
+  }
+
+
+  ORTHANC_PLUGINS_API const char* OrthancPluginGetVersion()
+  {
+    return ORTHANC_DICOM_WEB_VERSION;
+  }
+}
---
+++ Resources/SyncOrthancFolder.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python
+
+#
+# This maintenance script updates the content of the "Orthanc" folder
+# to match the latest version of the Orthanc source code.
+#
+
+import os
+import shutil
+import urllib2
+
+PLUGIN_SDK_VERSION = '0.9.1'
+
+SOURCE = '/home/jodogne/Subversion/Orthanc'
+TARGET = os.path.join(os.path.dirname(__file__), '..', 'Orthanc')
+REPOSITORY = 'https://bitbucket.org/sjodogne/orthanc/raw/Orthanc-%s/Plugins/Include' % PLUGIN_SDK_VERSION
+
+FILES = [
+    'Core/ChunkedBuffer.cpp',
+    'Core/ChunkedBuffer.h',
+    'Core/Enumerations.cpp',
+    'Core/Enumerations.h',
+    'Core/ImageFormats/ImageAccessor.cpp',
+    'Core/ImageFormats/ImageAccessor.h',
+    'Core/ImageFormats/ImageBuffer.cpp',
+    'Core/ImageFormats/ImageBuffer.h',
+    'Core/ImageFormats/PngReader.cpp',
+    'Core/ImageFormats/PngReader.h',
+    'Core/Logging.h',
+    'Core/OrthancException.cpp',
+    'Core/OrthancException.h',
+    'Core/PrecompiledHeaders.h',
+    'Core/Toolbox.cpp',
+    'Core/Toolbox.h',
+    'Plugins/Samples/Common/VersionScript.map',
+    'Resources/CMake/BoostConfiguration.cmake',
+    'Resources/CMake/Compiler.cmake',
+    'Resources/CMake/DownloadPackage.cmake',
+    'Resources/CMake/GoogleTestConfiguration.cmake',
+    'Resources/CMake/JsonCppConfiguration.cmake',
+    'Resources/CMake/LibPngConfiguration.cmake',
+    'Resources/CMake/PugixmlConfiguration.cmake',
+    'Resources/CMake/ZlibConfiguration.cmake',
+    'Resources/MinGW-W64-Toolchain32.cmake',
+    'Resources/MinGW-W64-Toolchain64.cmake',
+    'Resources/MinGWToolchain.cmake',
+    'Resources/ThirdParty/VisualStudio/stdint.h',
+    'Resources/WindowsResources.py',
+    'Resources/WindowsResources.rc',
+]
+
+SDK = [
+    'orthanc/OrthancCPlugin.h',
+]   
+
+
+for f in FILES:
+    source = os.path.join(SOURCE, f)
+    target = os.path.join(TARGET, f)
+    try:
+        os.makedirs(os.path.dirname(target))
+    except:
+        pass
+
+    shutil.copy(source, target)
+
+for f in SDK:
+    source = '%s/%s' % (REPOSITORY, f)
+    target = os.path.join(TARGET, 'Sdk-%s' % PLUGIN_SDK_VERSION, f)
+    try:
+        os.makedirs(os.path.dirname(target))
+    except:
+        pass
+
+    with open(target, 'w') as g:
+        g.write(urllib2.urlopen(source).read())
