d8772a7cd16c41782d81535dd13e738112c7020f
Sebastien Jodogne <s.jodogne@gmail.com>
2015-12-09 17:13:09 +0100

    fix static build

--- Plugin/WadoRs.cpp
+++ Plugin/WadoRs.cpp
@@ -20,13 +20,14 @@
 
 #include "Plugin.h"
 
-#include <boost/lexical_cast.hpp>
-
 #include "Configuration.h"
 #include "Dicom.h"
 #include "DicomResults.h"
 #include "../Orthanc/Core/Toolbox.h"
 #include "../Orthanc/Core/OrthancException.h"
+
+#include <boost/lexical_cast.hpp>
+#include <memory>
 
 static bool AcceptMultipartDicom(const OrthancPluginHttpRequest* request)
 {
